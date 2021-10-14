71c5dd4fec6c84f3e535fda10c9460a864c4d3e6
Горбулёв Сергей <gorbulevsv@yandex.ru>
2021-10-14 23:04:27 +0300

    Добавьте файлы проекта.

---
+++ wwwroot/my/ChangeTheme.js
@@ -0,0 +1,15 @@
+﻿var change_theme = function () {
+    //if (!$("body").hasClass('bg-dark text-white')) {
+    //    $("body").addClass('bg-dark text-white');
+    //    $(".navbar").removeClass("navbar-light bg-white text-dark");
+    //    $(".navbar").addClass("navbar-dark bg-dark text-white");
+    //}
+    //else {
+    //    $("body").removeClass('bg-dark text-white');
+    //    $(".navbar").removeClass("navbar-dark bg-dark text-white");
+    //    $(".navbar").addClass("navbar-light bg-white text-dark");
+    //}
+
+    if ($("link").attr("href") == "~/bootstrap/css/bootstrap.min.css") $("link").attr("href", "");
+    else $("link").attr("href","~/bootstrap/css/bootstrap.min.css");
+}
